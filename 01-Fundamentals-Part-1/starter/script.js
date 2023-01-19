b77bccd762f51318d67eff231de7e5df61a83409
Jun Zhao <junzhao1111@gmail.com>
2023-01-19 11:35:32 +0000

    Learnt the basic operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
@@ -46,4 +47,34 @@ const birthYear = 1991;
 
 var job = 'programmer';
 job = 'teacher';
+*/
 
+//Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2**3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+
+console.log(firstName + ' ' +  lastName);
+
+//Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
