553a862a508973219ecbceed2f8f025614c2dc4d
Jun Zhao <junzhao1111@gmail.com>
2023-01-22 12:32:32 +0000

    Completed challange 2

---
+++ 01-Fundamentals-Part-1/starter/Challenges.js
@@ -0,0 +1,39 @@
+//Challenge 1
+/*
+const markHeight = 1.69;
+const markMass = 78;
+
+const johnHeight = 1.95;
+const johnMass = 92;
+
+const markBMI = markMass / markHeight ** 2;
+
+const johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markHigherBMI);
+*/
+
+//Challenge 2
+
+/*
+const markHeight = 1.69;
+const markMass = 78;
+
+const johnHeight = 1.95;
+const johnMass = 92;
+
+const markBMI = markMass / markHeight ** 2;
+
+const johnBMI = johnMass / johnHeight ** 2;
+
+let markHigherBMI = markBMI > johnBMI;
+
+if(markHigherBMI == true){
+    console.log(`Marks BMI (${markBMI}) is higher than Johns BMI (${johnBMI})`)
+}else{
+    console.log(`Johns BMI (${johnBMI}) is higher than Marks BMI (${markBMI})`)
+}
+*/
+
