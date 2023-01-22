2bd5371107dccddbf85dd6d3730a6ba8db6fc76e
Jun Zhao <junzhao1111@gmail.com>
2023-01-22 17:40:20 +0000

    Learnt Equality and is not operators and else if.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -147,6 +147,7 @@ console.log('23' - '10' - 3);
 console.log('23' * '2');
 */
 
+/*
 // 5 falsey values: 0, '', undefined, null, NaN
 const money = 0;
 if(money){
@@ -161,3 +162,27 @@ if(height){
 }else{
     console.log('Height is UNDEFINED');
 }
+*/
+
+const age = '18';
+if( age === 18) console.log('You just became an adult (strict)');
+
+if( age == 18) console.log('You just became an adult (loose)');
+
+const favourite = Number(prompt("What's your favourite number"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite === 23) { 
+    console.log('cool! 23 is an amazing number!');
+} else if(favourite === 7){
+    console.log('7 is also a cool number');
+}else if(favourite === 9){
+    console.log('9 is also cool');
+}
+ else{
+    console.log('Numner is no 23 or 9 or 7!');
+}
+
+if(favourite !== 23) console.log('Why not 23?');
+
