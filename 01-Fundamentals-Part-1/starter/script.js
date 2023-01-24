9147649fb265c3615d7bd977b9c32aa8a22ad642
Jun Zhao <junzhao1111@gmail.com>
2023-01-24 11:42:39 +0000

    Learnt how to use swtich statments

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -188,6 +188,7 @@ if(favourite === 23) {
 if(favourite !== 23) console.log('Why not 23?');
 */
 
+/*
 const hasDriversLicense = true; //A
 const hasGoodVision = true; //B
 
@@ -209,5 +210,46 @@ if(hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Someone should drive...');
 }
+*/
+
+const day = 'thursday';
+
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break
+    case 'wednesday' || 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday' :
+        console.log('record, videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('enjoy the weekdend');
+        break;
+    default:
+        console.log('not a valid day');
+}   
+
+if(day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if(day === 'tuesday'){
+    console.log('Prepare theory videos');
+} else if(day === 'wednesday' || day == 'thursday' ){
+    console.log('Write code examples');
+} else if(day === 'friday'){
+    console.log('record, videos');
+}else if(day === 'saturday' || day === 'sunday'){
+    console.log('enjoy your weekend');
+} else{
+    console.log('not a valid day!');
+}
+
 
 
