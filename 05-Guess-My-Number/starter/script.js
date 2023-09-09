3d7ab7b47a777fd3a2583607b6506c2b85e14068
Jun Zhao <junzhao1111@gmail.com>
2023-09-09 14:15:55 +0100

    prints out value to console when button is pressed

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,18 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'correct number';
+// document.querySelector('.message').textContent = 'correct number';
+
+// document.querySelector('.number').textContent = 13;
+
+// document.querySelector('.score').textContent = 10;
+
+// /* .value sets the value for input fields */
+// document.querySelector('.guess').value = 23;
+
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
