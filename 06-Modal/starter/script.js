3d93e8c4acc3c53bba7eefd9ca9236466d3cab9a
Jun Zhao <junzhao1111@gmail.com>
2023-09-11 14:50:45 +0100

    created loop for openmode buttons

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const model = document.querySelector('.model');
+const overly = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+
+const btnOpenModel = document.querySelectorAll('.show-modal');
+console.log(btnOpenModel);
+for (let i = 0; i < btnOpenModel.length; i++) {
+  console.log(btnOpenModel[i].textContent);
+}
