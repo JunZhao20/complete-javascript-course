699eede47afb5bebb6faeb619f1d69f2cbdbb7e0
Jun Zhao <junzhao1111@gmail.com>
2023-09-11 14:28:07 +0100

    Finished Guess Number website

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,6 +13,54 @@
 
 // console.log(document.querySelector('.guess').value);
 
+let specialNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('⛔️ No Number');
+
+    // Win condition
+  } else if (guess === specialNumber) {
+    displayMessage('🎉 Correct number');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = specialNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (guess !== specialNumber) {
+    if (score > 1) {
+      // document.querySelector('.message').textContent =
+      // guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+      displayMessage(guess > specialNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // document.querySelector('.message').textContent = '💥 You lost the game!';
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  specialNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+  //location.reload();
 });
