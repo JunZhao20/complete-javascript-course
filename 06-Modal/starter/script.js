1a8650820c4b0d29836240e6548f8a436b23bbc7
Jun Zhao <junzhao1111@gmail.com>
2023-09-11 16:02:39 +0100

    Completed modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,11 +1,32 @@
 'use strict';
 
-const model = document.querySelector('.model');
-const overly = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 
 const btnOpenModel = document.querySelectorAll('.show-modal');
-console.log(btnOpenModel);
+
+const openModal = function () {
+  // hidden class attached to the modal window class
+  // classlist.removes additional classes within the class
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnOpenModel.length; i++) {
-  console.log(btnOpenModel[i].textContent);
+  btnOpenModel[i].addEventListener('click', openModal);
 }
+
+btnCloseModel.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
