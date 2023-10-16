8af77fdb72f77c3d98241f2e1c38021f10fe6c16
Jun Zhao <junzhao1111@gmail.com>
2023-10-16 12:42:41 +0100

    created Person constructor function

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+// Constructor function
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // DO NO CREATE FUNCTION IN CONSTRUCTOR FUNCTION
+};
+
+const jun = new Person('Jun', 2001);
+
+console.log(jun);
+
+// Prototypes
+
+// Linking Person class to prototype so it can access method within the prototype
+Person.prototype.calcAge = function () {
+  console.log(2023 - this.birthYear);
+};
+
+jun.calcAge();
+
+console.log();
