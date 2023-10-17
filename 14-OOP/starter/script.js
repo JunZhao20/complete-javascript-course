dbf0ad44c437a7dfd48322ea0e920d13f651690b
Jun <j.zhao1@newcastle.ac.uk>
2023-10-17 16:56:48 +0100

    Completed Challenge 2 OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -47,37 +47,99 @@ GOOD LUCK 😀
 // };
 
 // Car.prototype.brake = function () {
-//   return console.log(`${this.make} deccelerated to ${this.speed - 5}Km/h`);
+//   return console.log(`${this.make} decelerated to ${this.speed - 5}Km/h`);
 // };
 
 // const fordCar = new Car('ford', 10);
 // fordCar.accelerate();
 
-class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
+// class PersonCl {
+//   constructor(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   }
+
+//   calcAgeCl() {
+//     console.log(`${2023 - this.birthYear}`);
+//   }
+
+//   get name() {
+//     return this.firstName;
+//   }
+
+//   set age(num) {
+//     return (this.birthYear = num);
+//   }
+
+//   set firstName(name) {
+//     return (this._firstName = name);
+//   }
+
+//   static names() {
+//     return (this.firstName = 'tim');
+//   }
+// }
+
+// const jun1 = new PersonCl('jun1', 1001);
+// console.log(jun1.birthYear);
+// console.log((jun1.age = 2001));
+
+// console.log((jun1.firstName = 'hao'));
+
+/////// Example: Object.Create()
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2023 - this.birthYear);
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+
+// steven.birthYear = 2001;
+// steven.firstName = 'steven';
+// steven.calcAge();
+// console.log(steven);
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h 
+(but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+*/
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    return console.log(`${this.make} accelerated to ${this.speed + 10}Km/h`);
   }
 
-  calcAgeCl() {
-    console.log(`${2023 - this.birthYear}`);
+  brake() {
+    return console.log(`${this.make} decelerated to ${this.speed - 5}Km/h`);
   }
 
-  get name() {
-    return this.firstName;
+  get speedUS() {
+    return `Your ${this.make} is going ${(this.speed = this.speed / 1.6)}mi/h`;
   }
 
-  set age(num) {
-    return (this.birthYear = num);
-  }
-
-  set firstName(name) {
-    return (this._firstName = name);
+  set speedUS(mph) {
+    this.speed = mph * 1.6;
+    return this.speed;
   }
 }
 
-const jun1 = new PersonCl('jun1', 1001);
-console.log(jun1.birthYear);
-console.log((jun1.age = 2001));
+const fordCar = new Car('Ford', 120);
 
-console.log((jun1.firstName = 'hao'));
+console.log(fordCar.speedUS);
+fordCar.speedUS = 75;
+console.log(fordCar.speed);
+fordCar.accelerate();
+fordCar.brake();
