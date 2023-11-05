cb03e87a173ad7504e059f942fae076ffb64ce15
Jun <j.zhao1@newcastle.ac.uk>
2023-11-05 12:50:33 +0000

    testing Numeric separator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -264,3 +264,7 @@ console.log(Number.parseFloat('2.5rem'));
 console.log(Number.isNaN(Number.parseInt('a2')));
 
 console.log(Number.isNaN(+'2s'));
+
+// Numeric separator
+const d = 2_000_000_000;
+console.log(d);
